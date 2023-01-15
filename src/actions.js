cabf7ace2defd36e0771a818f2bb9922bb76618c
Vincent Fuertez <vfuertez@gmail.com>
2023-01-15 01:42:30 -0500

    update action added

--- src/actions.js
+++ src/actions.js
@@ -1,24 +1,45 @@
-import {redirect} from 'react-router-dom'
+import { redirect } from 'react-router-dom'
 
 const URL = 'https://cheeseapp-api-55ex.onrender.com'
 
-export const createAction = async ({request}) => {
+export const createAction = async ({ request }) => {
+  const formData = await request.formData()
 
-    const formData = await request.formData()
+  const newCheese = {
+    name: formData.get('name'),
+    image: formData.get('image'),
+    type: formData.get('type'),
+  }
 
-    const newCheese = {
-        name: formData.get('name'),
-        image: formData.get('image'),
-        type: formData.get('type')
-    }
-    
-    await fetch(URL + '/cheese', {
-        method: 'post',
-        headers: {
-            "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(newCheese)
-    })
-    
-    return redirect('/')
-    }
+  await fetch(URL + '/cheese', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newCheese),
+  })
+
+  return redirect('/')
+}
+
+/// Update ///
+
+export const updateAction = async ({ request, params }) => {
+  const formData = await request.formData()
+
+  const cheeseUpdate = {
+    name: formData.get('name'),
+    image: formData.get('image'),
+    title: formData.get('type'),
+  }
+
+  await fetch(URL + '/cheese/' + params.id, {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(cheeseUpdate),
+  })
+
+  return redirect('/')
+}
