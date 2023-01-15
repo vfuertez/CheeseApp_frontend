ff5a8e7a953ce8abbca61640b61eb34212b3a082
Vincent Fuertez <vfuertez@gmail.com>
2023-01-14 21:18:18 -0500

    actions.js fixed / imported createAction to router.js

--- src/actions.js
+++ src/actions.js
@@ -2,20 +2,23 @@ import {redirect} from 'react-router-dom'
 
 const URL = 'https://cheeseapp-api-55ex.onrender.com'
 
-const formData = await requestAnimationFrame.formData()
+export const createAction = async ({request}) => {
 
-const newCheese = {
-    name: formData.get('name'),
-    image: formData.get('image'),
-    type:formData.get('type')
-}
+    const formData = await request.formData()
 
-await fetch(URL + '/cheese', {
-    method: 'post',
-    headers: {
-        "Content-Type": 'application/json'
-    },
-    body: JSON.stringify(newCheese)
-})
-
-return redirect('/')
+    const newCheese = {
+        name: formData.get('name'),
+        image: formData.get('image'),
+        type: formData.get('type')
+    }
+    
+    await fetch(URL + '/cheese', {
+        method: 'post',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(newCheese)
+    })
+    
+    return redirect('/')
+    }
