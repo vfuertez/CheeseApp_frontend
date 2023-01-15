5d38558185df4f7c7820080afc65a131e0407db2
Vincent Fuertez <vfuertez@gmail.com>
2023-01-14 20:41:34 -0500

    added cheese display in Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
 
-function Index() {
-  return <div>Index</div>
+function Index(props) {
+  const cheeses = useLoaderData()
+
+  return cheeses.map(cheese => (
+    <div key={cheese._id} className="Index">
+      <Link to={`/${cheese._id}`}>
+        <h1>{cheese.name}</h1>
+      </Link>
+      <img src={cheese.image} alt={cheese.name} />
+    </div>
+  ))
 }
+
+
 export default Index
