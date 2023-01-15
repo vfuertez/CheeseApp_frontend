ea122d2627829245fe324d2dcb45d8e11fd7989c
Vincent Fuertez <vfuertez@gmail.com>
2023-01-15 01:38:19 -0500

    added update form in show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, Form } from 'react-router-dom'
 
 function Show(props) {
-    const cheese = useLoaderData()
+  const cheese = useLoaderData()
   return (
     <div>
-    <h1>{cheese.name}</h1>
-    <h2>{cheese.type}</h2>
-    <img src={cheese.image} alt={cheese.name} />
-  </div>
+      <Form action={`/update/${cheese._id}`} method="post">
+        <input type="input" name="name" placeholder="cheese name" />
+        <input type="input" name="image" placeholder="cheese image" />
+        <input type="input" name="type" placeholder="cheese type" />
+        <input type="submit" value="update" />
+      </Form>
+
+      <h1>{cheese.name}</h1>
+      <h2>{cheese.type}</h2>
+      <img src={cheese.image} alt={cheese.name} />
+    </div>
   )
 }
 export default Show
