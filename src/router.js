aacee2e63476750a7326f08a4efd8b33d5333342
Vincent Fuertez <vfuertez@gmail.com>
2023-01-14 20:02:22 -0500

    added functions on component/pages & updated router.js

--- src/router.js
+++ src/router.js
@@ -1,15 +1,22 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-  } from "react-router-dom"
-  import App from "./App"
-  
-  const router = createBrowserRouter(
-    createRoutesFromElements(
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+import App from './App'
+import Index from './pages/Index'
+import Show from './pages/Show'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <Route path="/" element={<App />}>
-        
-    </Route>)
+      <Route path="" element={<Index />} />
+      <Route path=":id" element={<Show />} />
+      <Route path="create" />
+      <Route path="update/:id" />
+      <Route path="delete/:id" />
+    </Route>
   )
-  
-  export default router
+)
+
+export default router
