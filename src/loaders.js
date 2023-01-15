9ef6b7c87e2dd00be30d722cc7031986f3c76596
Vincent Fuertez <vfuertez@gmail.com>
2023-01-15 01:33:21 -0500

    created show page

--- src/loaders.js
+++ src/loaders.js
@@ -5,3 +5,9 @@ export const cheesesLoader = async () => {
     const cheeses = await response.json()
     return cheeses
 }
+
+export const quesoLoader = async ({params}) => {
+    const response = await fetch(URL + "/cheese/" + params.id )
+    const queso = await response.json()
+    return queso
+}
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useLoaderData } from "react-router-dom"
 
-function Show() {
-  return <div>Show</div>
+function Show(props) {
+    const cheese = useLoaderData()
+  return (
+    <div>
+    <h1>{cheese.name}</h1>
+    <h2>{cheese.type}</h2>
+    <img src={cheese.image} alt={cheese.name} />
+  </div>
+  )
 }
 export default Show
